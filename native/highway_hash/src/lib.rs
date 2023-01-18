85a50e14052d3568795752ca7c5dfc01b0ec73a4
Jason Pollentier <jason.pollentier@revelry.co>
2023-01-17 19:13:31 -0600

    Create the wrapper function for the HighwayHash library

--- native/highway_hash/src/lib.rs
+++ native/highway_hash/src/lib.rs
@@ -1,6 +1,10 @@
+use highway::{HighwayHash, Key, PortableHash};
+
 #[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+fn hash64(data: String) -> u64 {
+  // NOTE: using a dummy key here just so we can validate against the unit test
+  // https://github.com/nickbabcock/highway-rs/blob/master/tests/hash.rs#L4
+  return PortableHash::new(Key([0,0,0,0])).hash64(data.as_bytes());
 }
 
-rustler::init!("Elixir.NifExample.HighwayHash", [add]);
+rustler::init!("Elixir.NifExample.HighwayHash", [hash64]);
